8588adb5077e6dd7920209a4315cb12b868cdbde
Alliaance360 Club <info@alliance360.club>
2025-06-01 12:29:33 -0400

    minor change to favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600&display=swap" rel="stylesheet" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/img//apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img//favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/img//favicon-16x16.png" />
+        <link rel="manifest" href="/assets/img//site.webmanifest" />
       </head>
       <body>
         {/* Wrap everything in ThirdwebProvider */}
