fa1d57706677e449df4c0fd5f2d4c778d19eb72a
Alliaance360 Club <info@alliance360.club>
2025-06-01 11:02:23 -0400

    minor UI

--- app/components/Home/StatsSection.tsx
+++ app/components/Home/StatsSection.tsx
@@ -1,9 +1,9 @@
 export default function StatsSection() {
   const stats = [
-    { title: 'Smart Wallet (crypto)', value: '$ 0.00', color: 'text-success' },
-    { title: 'NFT Value', value: '$ 0.00', color: 'text-success' },
-    { title: 'Cashback Card Balance', value: '$ 0.00', color: 'text-success' },
-    { title: 'Commissions to come', value: '$ 0.00', color: 'text-warning' },
+    { title: 'Smart Wallet', value: '0.00', color: 'text-success' },
+    { title: 'NFT Value', value: '0.00', color: 'text-success' },
+    { title: 'Cashback Card', value: '0.00', color: 'text-success' },
+    { title: 'Commissions', value: '0.00', color: 'text-warning' },
   ];
 
   return (
